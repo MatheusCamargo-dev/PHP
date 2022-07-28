3847bc371af1741b5a3638c55efb37f96f55c245
Matheus Romeiro Camargo <matheuscamargodev@gmail.com>
2022-07-28 12:40:17 -0300

    functions_math

--- native_functions.php
+++ native_functions.php
@@ -12,7 +12,7 @@
 </head>
 <body>
     <?php 
-        // funções nativa do php para uso
+        // funções nativa do php para uso de texto
         $texto = 'matheus';
         // string to lower strtolower($texto) -> Transforma todos os caracteres da string em minúsculos
         echo strtolower($texto);
@@ -42,6 +42,45 @@
         // substr($texto, <posição inicial>, <quantidade de caracteres>) -> Retorna parte de uma string
         // texto é tratado como uma array de caracteres iniciando por 0..1..2..3..4..5
         echo substr($texto, 2, 5);
+        
+        echo '<br>';
+
+        // Funções nativas do php para tarefas matemáticas
+        $numero = 7.50;
+
+        // ceil($numero) -> arredonda o valor para cima
+        echo ceil($numero);
+        
+        echo '<br>';
+
+        //floor($numero) -> arredonda o valor para baixo
+        echo floor($numero);
+
+        echo '<br>';
+
+        // round($numero) -> arredonda o valor com base nas casas decimais
+        // >= 50 arredonda pra cima 49<= arredonda para baixo
+        echo round($numero);
+        
+        echo '<br>';
+
+        // rand(x, y) -> gera os valores entre 'x' e 'y'
+        // rand() -> gera um número aleatório de 0 até getrandmax()
+        echo rand(1, 10);
+        echo '<br>';
+        echo getrandmax();
+        echo '<br>';
+
+
+        // sqrt($numero) -> retorna a raiz quadra de um número
+        echo sqrt($numero);
+
+        echo '<br>';
+
+        //number_format -> formata as casas decimais de um número
+        echo number_format((sqrt($numero)), 2, ',','.');
+        
+        echo '<br>';
         
 
     ?>
