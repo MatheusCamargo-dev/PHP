6e02c589bf0f5a1dff6de56d5924c4b123d2ec78
Matheus Romeiro Camargo <matheuscamargodev@gmail.com>
2022-08-01 17:45:27 -0300

    Update teste7.php

--- teste_logica/teste7.php
+++ teste_logica/teste7.php
@@ -18,6 +18,7 @@
            Suponha que você precise criar um aplicativo de entregas. Em seu aplicativo, as entregas realizadas em um raio de 2KM não são cobradas. As entregas realizadas a partir de 2KM até 15KM são cobradas 1,15 R$ por KM, e a partir de 15 KM existe um custo de entrega fixo de 17,50 R$. Escreva uma função que receba a distancia em KM e calcule o valor da entrega. 
             */
 
+            // função que retorna o valor de entrega de acordo com o KM passado como paramêtro
             function valorDeEntrega($km){
                 if ($km <= 2){
                     return 0;
