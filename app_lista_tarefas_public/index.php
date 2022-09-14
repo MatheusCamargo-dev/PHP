6afbaa055e05dd3daa10a207178a428a85dc989c
Matheus Romeiro Camargo <matheuscamargodev@gmail.com>
2022-09-14 16:40:40 -0300

    push

--- app_lista_tarefas_public/index.php
+++ app_lista_tarefas_public/index.php
@@ -7,73 +7,7 @@
 		<link rel="stylesheet" href="css/estilo.css">
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
-		<script>
-			function requisitarPagina(url){
-				document.getElementById('conteudo').innerHTML = ''
-				if(!document.getElementById('loading')){
-					let imgLoading = document.createElement('img')
-					imgLoading.id = 'loading'
-					imgLoading.src = './img/loading.gif'
-					imgLoading.className = 'rounded mx-auto d-block'
-					document.getElementById('conteudo').appendChild(imgLoading)
-					
-				}
-
-				let active_1 = document.getElementById('active_1')
-				let active_2 = document.getElementById('active_2')
-				let active_3 = document.getElementById('active_3')
-				let inserido = document.getElementById('inserido')
-
-				if(url == 'tarefas_pendentes.php'){
-					active_1.className = 'list-group-item active'
-					active_2.className = 'list-group-item'
-					active_3.className = 'list-group-item'
-					if(document.getElementById('inserido') != null){
-						inserido.className = ''
-						inserido.innerHTML = ''
-					}
-				}
-				if(url == 'nova_tarefa.php'){
-					active_1.className = 'list-group-item'
-					active_2.className = 'list-group-item active'
-					active_3.className = 'list-group-item'
-					if(document.getElementById('inserido') != null){
-						inserido.className = ''
-						inserido.innerHTML = ''
-					}
-
-				}
-				if(url == 'todas_tarefas.php'){
-					active_1.className = 'list-group-item'
-					active_2.className = 'list-group-item'
-					active_3.className = 'list-group-item active'
-					if(document.getElementById('inserido') != null){
-						inserido.className = ''
-						inserido.innerHTML = ''
-					}
-
-
-				}
-
-				let ajax = new XMLHttpRequest();
-
-				ajax.open('GET', url)
-				ajax.onreadystatechange = function() {
-					if(ajax.readyState == 4 && ajax.status == 200){
-						document.getElementById('conteudo').innerHTML = ajax.responseText
-						console.log(ajax.status)
-						console.log('ajax.status')
-
-					}else if (ajax.readyState == 4 && ajax.status == 404){
-						document.getElementById('conteudo').innerHTML = 'requisição finalizada, mas não encontrou o recurso desejado'
-						console.log(ajax.status)
-					}
-				}
-				
-				ajax.send()
-			}
-
-		</script>
+		<script src="./script.js"></script>
 	</head>
 
 	<body>
---
+++ app_lista_tarefas_public/script.js
@@ -0,0 +1,90 @@
+function requisitarPagina(url){
+    document.getElementById('conteudo').innerHTML = ''
+    if(!document.getElementById('loading')){
+        let imgLoading = document.createElement('img')
+        imgLoading.id = 'loading'
+        imgLoading.src = './img/loading.gif'
+        imgLoading.className = 'rounded mx-auto d-block'
+        document.getElementById('conteudo').appendChild(imgLoading)
+        
+    }
+
+    let active_1 = document.getElementById('active_1')
+    let active_2 = document.getElementById('active_2')
+    let active_3 = document.getElementById('active_3')
+    let inserido = document.getElementById('inserido')
+
+    if(url == 'tarefas_pendentes.php'){
+        active_1.className = 'list-group-item active'
+        active_2.className = 'list-group-item'
+        active_3.className = 'list-group-item'
+        if(document.getElementById('inserido') != null){
+            inserido.className = ''
+            inserido.innerHTML = ''
+        }
+    }
+    if(url == 'nova_tarefa.php'){
+        active_1.className = 'list-group-item'
+        active_2.className = 'list-group-item active'
+        active_3.className = 'list-group-item'
+        if(document.getElementById('inserido') != null){
+            inserido.className = ''
+            inserido.innerHTML = ''
+        }
+
+    }
+    if(url == 'todas_tarefas.php'){
+        active_1.className = 'list-group-item'
+        active_2.className = 'list-group-item'
+        active_3.className = 'list-group-item active'
+        if(document.getElementById('inserido') != null){
+            inserido.className = ''
+            inserido.innerHTML = ''
+        }
+
+
+    }
+
+    let ajax = new XMLHttpRequest();
+
+    ajax.open('GET', url)
+    ajax.onreadystatechange = function() {
+        if(ajax.readyState == 4 && ajax.status == 200){
+            document.getElementById('conteudo').innerHTML = ajax.responseText
+            console.log(ajax.status)
+            console.log('ajax.status')
+
+        }else if (ajax.readyState == 4 && ajax.status == 404){
+            document.getElementById('conteudo').innerHTML = 'requisição finalizada, mas não encontrou o recurso desejado'
+            console.log(ajax.status)
+        }
+    }
+    
+    ajax.send()
+}
+function editar(id) {
+
+    let form = document.createElement('form')
+    form.action = '#'
+    form.method = 'POST'
+
+    let inputTarefa = document.createElement('input')
+    inputTarefa.type = 'text'
+    inputTarefa.name = 'tarefa'
+    inputTarefa.className = 'form-control'
+
+    let button = document.createElement('button')
+    button.type = 'submit'
+    button.className = 'btn btn-info'
+    button.innerHTML = 'Atualizar'
+
+    form.appendChild(inputTarefa)
+    form.appendChild(button)
+
+
+    let tarefa_id = document.getElementById('tarefa_'+id)
+aaaaaaaa
+    // tarefa_id.innerHTML = ''
+
+    tarefa_id.insertBefore(form, tarefa)
+}
