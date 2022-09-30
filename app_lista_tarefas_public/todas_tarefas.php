39485aac9bfae5843ede0fd1012190b377ae816f
Matheus Romeiro Camargo <matheuscamargodev@gmail.com>
2022-09-30 17:23:42 -0300

    ajuste

--- app_lista_tarefas_public/todas_tarefas.php
+++ app_lista_tarefas_public/todas_tarefas.php
@@ -10,7 +10,7 @@
             <?php echo $tarefa->tarefa; ?> <?php echo $tarefa->status;?>
         </div>
         <div class="col-sm-3 mt-2 d-flex justify-content-between">
-            <i class="fas fa-trash-alt fa-lg text-danger" onclick="remover(<?php echo $tarefa->id ?>, '<?= $tarefa->tarefa?>')"></i>
+            <i class="fas fa-trash-alt fa-lg text-danger" <?= $tarefa->status == 'realizado' ? 'style="margin-left:7.8vw;"' : ''?> onclick="remover(<?php echo $tarefa->id ?>, '<?= $tarefa->tarefa?>')"></i>
             <?php if($tarefa->status == 'pendente'){ ?>
             <i class="fas fa-edit fa-lg text-info" onclick="editar(<?php echo $tarefa->id?> , '<?php print $tarefa->tarefa ?>')"></i>
             <i class="fas fa-check-square fa-lg text-success" onclick="marcarRealizado(<?php echo $tarefa->id?>)"></i>
